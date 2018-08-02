8ac5c2f225f8bb0783135fc1fab6e0ba0aa0b92f
Can Serif Mekik <can.mekik@gmail.com>
2018-08-01 21:05:16 -0400

    documented subject.py

--- pyClarion/base/subject.py
+++ pyClarion/base/subject.py
@@ -2,6 +2,10 @@
 
 Subsystems house and manage functionally distinct collections of activation 
 channels. They control activation cycles and learning.
+
+WARNING:
+    At this point in time, the classes defined here are very experimental. The 
+    abstractions may change drastically. 
 """
 
 
@@ -23,6 +27,13 @@ class Statistic(abc.ABC):
 
 
 class Component(abc.ABC):
+    """An abstraction for managing some class of activation channels associated 
+    with a subsystem.
+
+    Components are abstractions meant to capture learning and forgetting 
+    routines. They monitor the activity of the subsystem to which they belong 
+    and modify its members (channels and/or parameters).
+    """
     pass
 
 
@@ -33,16 +44,24 @@ class Subsystem(abc.ABC):
     @property
     @abc.abstractmethod
     def channels(self) -> activation.ChannelSet:
+        """A set of activation channels representing top- and bottom-level 
+        knowledge stored in self.
+        """
         pass
 
     @property
     @abc.abstractmethod
     def components(self) -> T.Set[Component]:
+        """A set of subsystem components for handling learning, forgetting and 
+        other changes to the knowledge-base of the subject.
+        """
         pass
 
     @property
     @abc.abstractmethod
     def actions(self) -> node.Chunk2Callable:
+        """An object binding chunks to actions executable by this subsystem.
+        """
         pass
 
     @abc.abstractmethod
@@ -50,12 +69,30 @@ class Subsystem(abc.ABC):
         self, 
         input_map : node.Node2Float
     ) -> None:
+        """Run through one processing cycle of self.
+        """
         pass
 
 
 class Subject(object):
+    """An abstraction for representing Clarion agents.
 
+    Subject objects facilitate the interface between subsystems and the 
+    environment. The main responsibility of these objects is to distribute 
+    sensory input to subsystems. They also serve to bind together all 
+    subsystems associated with a given subject.
+
+    It may also be useful to define action callbacks affecting the environment 
+    as methods of this class. Action-centered subsystems would be passed sets 
+    of these methods as the callbacks to execute following an action decision. 
+    Internal actions may be defined within the relevant subsystem class 
+    definition and passed to relevant subsystems in the same way.
+    """
+
+    @abc.abstractmethod
     def __call__(self, input_map : node.Node2Float) -> None:
+        """Receive and process a new set of sensory/world information.
+        """
         pass
 
 
@@ -63,6 +100,9 @@ def execute_actions(
     actionable_chunks : node.ChunkSet, action_map : node.Chunk2Callable
 ): 
     """Execute actionable chunks.
+
+    Action execution used to be handled by a dedicated class. In hindsight, 
+    this was probably a better design. It will be readopted in a later version.
     """
 
     for chunk in actionable_chunks:
