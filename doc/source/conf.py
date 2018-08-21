5c6ab75a86153e5a0bdd53fd4be27a6a605664f1
Can Serif Mekik <can.mekik@gmail.com>
2018-08-21 18:50:24 -0400

    fix to doc/source/conf.py

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -31,7 +31,6 @@ release = '0.9.0'
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.doctest',
-    'sphinx_autodoc_typehints',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
