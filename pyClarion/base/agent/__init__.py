ca72882809fe4efd6e58004122aa48bcf83cd636
Can Serif Mekik <can.mekik@gmail.com>
2018-08-21 15:55:05 -0400

    Cleaned up repo for in preparation for RCOS

--- pyClarion/base/agent/__init__.py
+++
@@ -1,6 +0,0 @@
-"""This module provides abstractions for constructing Clarion subjects.
-
-WARNING:
-    At this point in time, most classes defined here are very experimental. The 
-    abstractions may change drastically. 
-"""
