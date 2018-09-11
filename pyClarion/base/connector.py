8ae614521a6bc41943d416c757221b678177672e
Can Serif Mekik <can.mekik@gmail.com>
2018-09-11 07:11:32 -0400

    connector.py: merged SelectorConnector, EffectorConnector as Actuator

--- pyClarion/base/connector.py
+++ pyClarion/base/connector.py
@@ -1,21 +1,15 @@
-'''
-Tools for propagating information within a Clarion agent.
+"""
+Tools for propagating information within a pyClarion network.
 
 Usage
 =====
 
-This module defines four concrete classes: ``NodeConnector``, 
-``ChannelConnector``, ``SelectorConnector``, and ``EffectorConnector``. 
-``NodeConnector`` instances are charged with determining and relaying the output 
-of their client nodes to listeners. ``ChannelConnectors`` are charged with 
-feeding inputs to client ``Channel`` instances and relaying their outputs to 
-listeners. 
+This module defines three concrete classes: ``NodeConnector``, ``FlowConnector`` 
+and ``Actuator``. 
 
-Listeners to ``NodeConnector`` instances are expected to be either 
-``ChannelConnector`` or ``SelectorConnector`` instances. ``NodeConnector`` 
-instances are expected to only listen to ``ChannelConnector`` instances, and 
-``EffectorConnector`` instances are expected to only listen to 
-``SelectorConnector`` instances.
+``NodeConnector`` instances relay node activations to flows and selectors. 
+``FlowConnector`` objects relay transformed activations back to listener nodes. 
+``Actuator`` objects select and execute actions based on node activations.
 
 Example
 -------
@@ -41,7 +35,7 @@ This example is adapted builds on an example from the documentation of
 ...     pass
 ... 
 >>> class FineGrainedTopDownChannel(Channel[MyPacket]):
-...     """Represents a top-down link between two individual nodes."""
+...     '''Represents a top-down link between two individual nodes.'''
 ... 
 ...     def __init__(self, chunk, microfeature, weight):
 ...         self.chunk = chunk
@@ -128,7 +122,7 @@ Users are not expected to directly implement the abstract ``__call__`` and
 ``propagate`` methods of these classes. Instead, they should work with the 
 concrete classes provided by this module.
 
-'''
+"""
 
 
 import abc
@@ -157,13 +151,13 @@ Ot = TypeVar('Ot', bound=Packet)
 ################
 
 class Connector(Generic[It], abc.ABC):
-    '''
+    """
     Connects client to relevant constructs as a listener.
 
     This is an abstract class, it cannot be directly instantiated.
 
     For details, see module documentation.
-    '''
+    """
 
     def __init__(self) -> None:
 
@@ -192,13 +186,13 @@ class Connector(Generic[It], abc.ABC):
 
 
 class Propagator(Connector[It], Generic[It, Ot]):
-    '''
-    Allows client to push activation packets to listeners.
+    """
+    Allows listeners to pull output of client.
 
     This is an abstract class, it cannot be directly instantiated.
 
     For details, see module documentation.
-    '''
+    """
     
     def __init__(self, junction: Junction) -> None:
         
@@ -207,18 +201,19 @@ class Propagator(Connector[It], Generic[It, Ot]):
         self.output_buffer : Ot = self.propagate()
 
     def __call__(self) -> None:
-        '''Update ``self.output_buffer``.'''
+        """Update ``self.output_buffer``."""
 
         self.pull()        
         self.output_buffer = self.propagate()
 
     @abc.abstractmethod
     def propagate(self) -> Ot:
-        '''
+        """
         Compute and return current output of ``self.client``.
         
         This is an abstract method.
-        '''
+        """
+
         pass
 
     @abc.abstractmethod
@@ -238,7 +233,7 @@ class ActivationPropagator(Propagator[ActivationPacket, ActivationPacket], Gener
         return self.get_output
 
     def get_output(self, nodes: Iterable[Node] = None) -> ActivationPacket:
-        '''
+        """
         Return current output of ``self``.
 
         Returns a subpacket of ``self.output_buffer``. Intended to be used as a 
@@ -246,7 +241,7 @@ class ActivationPropagator(Propagator[ActivationPacket, ActivationPacket], Gener
 
         :param nodes: Nodes to be included in returned subpacket. If ``None``, 
             all nodes in ``self.output_buffer`` will be included. 
-        '''
+        """
 
         if not nodes:
             nodes = self.output_buffer.keys()
@@ -259,10 +254,11 @@ class ActivationPropagator(Propagator[ActivationPacket, ActivationPacket], Gener
 
 
 class NodeConnector(ActivationPropagator[Node]):
-    '''Embeds a node in a network.
+    """
+    Embeds a node in a network.
 
     For details, see module documentation.
-    '''
+    """
 
     def pull(self) -> None:
         
@@ -271,59 +267,54 @@ class NodeConnector(ActivationPropagator[Node]):
         ]
 
     def propagate(self) -> ActivationPacket:
-        '''Compute and return current output of client node.
-
-        Passes activation packets stored in ``self.buffer`` through 
-        ``self.junction`` and returns the result.
-        '''
+        """Compute and return current output of client node."""
         
         return self.junction(*self.input_buffer)
 
 
 class FlowConnector(ActivationPropagator[Flow]):
-    '''Embeds an activation channel in a Clarion agent.
+    """
+    Embeds an activation flow in a Clarion agent.
 
     For details, see module documentation.
-    '''
+    """
 
     def __init__(
         self, client: Flow, channel: Channel, junction: Junction
     ) -> None:
-    
+
         self.channel = channel
         super().__init__(client, junction)
 
 
     def propagate(self) -> ActivationPacket:
-        '''Update listeners with new output of client activation channel.
-
-        Passes activation packets stored in ``self.buffer`` through 
-        ``self.junction`` and feeds result to ``self.client``. Returns the 
-        output of ``self.clent``.
-        '''
+        """Compute and return output of client activation flow."""
 
         return self.channel(self.junction(*self.input_buffer))
 
 
-class SelectorConnector(Propagator[ActivationPacket, SelectorPacket]):
-    '''
-    Embeds an action selector in a Clarion agent.
+class Actuator(Propagator[ActivationPacket, SelectorPacket]):
+    """
+    Embeds an action selector/effector pair in a Clarion agent.
 
     For details, see module documentation.
-    '''
+    """
 
-    def __init__(self, selector: Selector, junction: Junction) -> None:
+    def __init__(
+        self, selector: Selector, effector: Effector, junction: Junction
+    ) -> None:
 
-        super().__init__(junction)
         self.selector = selector
+        self.effector = effector
+        super().__init__(junction)
+
+    def __call__(self):
+
+        super().__call__()
+        self.effector(self.get_output())
 
     def propagate(self) -> SelectorPacket:
-        '''Update listeners with new output of client activation channel.
-
-        Passes activation packets stored in ``self.buffer`` through 
-        ``self.junction`` and feeds result to ``self.client``. Returns the 
-        output of ``self.clent``.
-        '''
+        """Compute and return output of client selector."""
 
         return self.selector(self.junction(*self.input_buffer))
 
@@ -332,29 +323,11 @@ class SelectorConnector(Propagator[ActivationPacket, SelectorPacket]):
         return self.get_output
 
     def get_output(self, nodes: Iterable[Node] = None) -> SelectorPacket:
-        '''
+        """
         Return current output of ``self``.
 
         Returns a deepcopy of ``self.output_buffer``. Intended to be used as a 
         pull method for listeners of this propagator. 
-        '''
+        """
 
         return copy.deepcopy(self.output_buffer)
-
-
-class EffectorConnector(Connector[SelectorPacket]):
-    '''
-    Embeds an action effector in a Clarion agent.
-
-    For details, see module documentation.
-    '''
-
-    def __init__(self, effector: Effector) -> None:
-
-        super().__init__()
-        self.effector = effector
-
-    def __call__(self) -> None:
-
-        self.pull()
-        self.effector(*self.input_buffer)
