87fb23e808f12f43f90eda4a8e4d46fe28748021
Can Serif Mekik <can.mekik@gmail.com>
2019-03-17 13:32:44 -0400

    packets.py rewrite

--- pyClarion/base/packets.py
+++ pyClarion/base/packets.py
@@ -1,49 +1,97 @@
 """Tools for representing information about node strengths and decisions."""
 
 
-from typing import Any, Mapping, Collection, Tuple, NamedTuple, Union, cast 
-from types import MappingProxyType
-from pyClarion.base.symbols import ConstructSymbol, ConstructType
+from typing import Any, Mapping, Set, FrozenSet 
+from pyClarion.base.symbols import ConstructSymbol
 
 
 __all__ = ["ActivationPacket", "DecisionPacket"]
 
 
-####################
-### Type Aliases ###
-####################
+class _Packet(object):
+
+    def __init__(self, strengths: Mapping[ConstructSymbol, Any]) -> None:
+
+        self._strengths = dict(strengths)
+
+    def __repr__(self):
+
+        return "<{}: {}>".format(self.__class__.__name__, self._contents_repr())
+
+    def __contains__(self, key):
+
+        return key in self._strengths
+
+    def __iter__(self):
+
+        return iter(self._strengths)
+
+    def __len__(self):
+
+        return len(self._strengths)
+
+    def __getitem__(self, key: ConstructSymbol):
+
+        return self._strengths[key]
+
+    def get(self, key, default=None):
+
+        return self._strengths.get(key, default)
+
+    def keys(self):
+        """Return a view of keys in self."""
+
+        return self._strengths.keys()
+
+    def values(self):
+        """Return a view of values in self."""
+
+        return self._strengths.values()
+
+    def items(self):
+        """Return a view of items in self."""
+
+        return self._strengths.items()
+
+    def _contents_repr(self):
+
+        return "strengths={}".format(repr(self._strengths))
 
 
-ConstructSymbolMapping = Mapping[ConstructSymbol, Any]
-ConstructSymbolCollection = Collection[ConstructSymbol]
-
-
-###################
-### Definitions ###
-###################
-
-
-class ActivationPacket(NamedTuple):
+class ActivationPacket(_Packet):
     """
     Represents node strengths.
     
     :param strengths: Mapping of node strengths.
-    :param origin: Construct symbol identifying source of activation packet.
     """
 
-    strengths: ConstructSymbolMapping
-    origin: ConstructSymbol
+    pass
 
 
-class DecisionPacket(NamedTuple):
+class DecisionPacket(_Packet):
     """
-    Represents the result of an appraisal.
+    Represents a response.
 
     :param strengths: Mapping of node strengths.
-    :param chosen: Collection of selected actionable nodes.
-    :param origin: Construct symbol identifying source of activation packet.
+    :param selection: Set of selected actionable nodes.
     """
 
-    strengths: ConstructSymbolMapping
-    chosen: ConstructSymbolCollection
-    origin: ConstructSymbol
+    def __init__(
+        self, 
+        strengths: Mapping[ConstructSymbol, Any], 
+        selection: Set[ConstructSymbol]
+    ) -> None:
+
+        super().__init__(strengths)
+        self._selection = frozenset(selection)
+
+    @property
+    def selection(self) -> FrozenSet[ConstructSymbol]:
+
+        return self._selection
+
+    def _contents_repr(self):
+
+        strengths_repr = super()._contents_repr()
+        selection_repr = "selection={}".format(repr(self._selection))
+        return " ".join([strengths_repr, selection_repr])
