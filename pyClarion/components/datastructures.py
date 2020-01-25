1171b54f81072d84a01f4309f116578ec7502973
Can Serif Mekik <can.mekik@gmail.com>
2020-01-24 21:57:33 -0500

    added Map2VectorEncoder

--- pyClarion/components/datastructures.py
+++ pyClarion/components/datastructures.py
@@ -541,3 +541,42 @@ class MatchSpec(object):
             self.constructs ^= set(constructs)
         if predicates is not None:
             self.predicates ^= set(predicates)
+
+
+class Map2VectorEncoder(object):
+    """
+    Converts between activation maps and vectors for neural network I/O.
+
+    Consistently encodes activation maps as vectors and vectors as activation 
+    maps.
+    """
+
+    def __init__(self, encoding, default_strength=0.0):
+        """
+        Initialize a microfeature to vector converter.
+
+        :param encoding: Expected nodes in ordered iterable.
+        :param default_strength: The default activation strength.
+        """
+
+        self.encoding = encoding
+        self.default_strength = default_strength
+
+    def encode(self, activation_map):
+
+        vector = []
+        for node in self.encoding:
+            vector.append(activation_map.get(node, self.default_strength))
+        return vector
+
+    def decode(self, vector):
+        """
+        Decode activation vector into output node activation map.
+
+        :param vector: An activation vector.
+        """
+
+        output = {}
+        for index, activation in enumerate(vector):
+            output[self.encoding[index]] = activation
+        return output
