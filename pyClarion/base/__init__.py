9de5e6ff00830d05f5c0b7a1bad2bb091cc062e1
Can Serif Mekik <can.mekik@gmail.com>
2020-06-16 09:43:46 -0400

    rm base/utils.py

--- pyClarion/base/__init__.py
+++ pyClarion/base/__init__.py
@@ -6,7 +6,6 @@ To study the associated code follow this order:
     2. packets.py
     3. propagators.py
     4. realizers.py
-    5. utils.py
 """
 
 
@@ -14,4 +13,3 @@ from pyClarion.base.symbols import *
 from pyClarion.base.packets import *
 from pyClarion.base.propagators import *
 from pyClarion.base.realizers import *
-from pyClarion.base.utils import *
--- pyClarion/base/symbols.py
+++ pyClarion/base/symbols.py
@@ -1,7 +1,11 @@
 """Provides tools for naming, indexing, and selecting constructs."""
 
 
-__all__ = ["ConstructType", "ConstructSymbol", "FeatureSymbol", "MatchSpec"]
+__all__ = [
+    "ConstructType", "ConstructSymbol", "FeatureSymbol", "MatchSpec",
+    "feature", "chunk", "flow_bt", "flow_tb", "flow_tt", "flow_bb", 
+    "response", "buffer", "subsystem", "agent"
+]
 
 
 from enum import Flag, auto
@@ -329,3 +333,112 @@ class MatchSpec(object):
             self.constructs ^= set(constructs)
         if predicates is not None:
             self.predicates ^= set(predicates)
+
+
+##################################
+### Construct Symbol Factories ###
+##################################
+
+# These are convenience functions for constructing construct symbols.
+# They simply wrap the appropriate ConstructSymbol constructor.
+
+
+def feature(dim: Hashable, val: Hashable, lag: Hashable=0) -> ConstructSymbol:
+    """
+    Return a new feature symbol.
+
+    :param dim: Dimension of feature.
+    :param val: Value of feature.
+    """
+
+    return FeatureSymbol(dim, val, lag)
+
+
+def chunk(name: Hashable) -> ConstructSymbol:
+    """
+    Return a new chunk symbol.
+
+    :param cid: Chunk identifier.
+    """
+
+    return ConstructSymbol(ConstructType.chunk, name)
+
+
+def flow_bt(name: Hashable) -> ConstructSymbol:
+    """
+    Return a new bottom-up flow symbol.
+
+    :param cid: Name of flow.
+    """
+
+    return ConstructSymbol(ConstructType.flow_bt, name)
+
+
+def flow_tb(name: Hashable) -> ConstructSymbol:
+    """
+    Return a new top-down flow symbol.
+
+    :param cid: Name of flow.
+    """
+
+    return ConstructSymbol(ConstructType.flow_tb, name)
+
+
+def flow_tt(name: Hashable) -> ConstructSymbol:
+    """
+    Return a new top-level flow symbol.
+
+    :param cid: Name of flow.
+    """
+
+    return ConstructSymbol(ConstructType.flow_tt, name)
+
+
+def flow_bb(name: Hashable) -> ConstructSymbol:
+    """
+    Return a new bottom-level flow symbol.
+
+    :param cid: Name of flow.
+    """
+
+    return ConstructSymbol(ConstructType.flow_bb, name)
+
+
+def response(name: Hashable) -> ConstructSymbol:
+    """
+    Return a new response symbol.
+
+    :param name: Name of response.
+    """
+
+    return ConstructSymbol(ConstructType.response, name)
+
+
+def buffer(name: Hashable) -> ConstructSymbol:
+    """
+    Return a new buffer symbol.
+
+    :param name: Name of buffer.
+    """
+
+    return ConstructSymbol(ConstructType.buffer, name)
+
+
+def subsystem(name: Hashable) -> ConstructSymbol:
+    """
+    Return a new subsystem symbol.
+
+    :param name: Name of subsystem.
+    """
+
+    return ConstructSymbol(ConstructType.subsystem, name)
+
+
+def agent(name: Hashable) -> ConstructSymbol:
+    """
+    Return a new agent symbol.
+
+    :param name: Name of agent.
+    """
+
+    return ConstructSymbol(ConstructType.agent, name)
