40ebd28168f81e4589417302b667cfec9900f645
Joe Chu <chujoe26@gmail.com>
2021-03-30 18:45:04 -0400

    Removed unnecessary tape usage

--- tests/test_numdicts/test_numdicts.py
+++ tests/test_numdicts/test_numdicts.py
@@ -19,48 +19,40 @@ def linspace(a, b):
 class TestNumdictsAddition(unittest.TestCase):
     def test_addition_basic_functionality_defaults(self):
         for i, j in linspace(-10, 10):
-            tape = nd.GradientTape()
-            with tape:
-                # testing basic functionality for defaults
-                d1 = nd.NumDict(default=i)
-                d2 = nd.NumDict(default=j)
-                self.assertAlmostEqual((d1+d2).default, d1.default+d2.default)
-                d3 = nd.NumDict(default=3.0)
-                self.assertAlmostEqual((d2+d1+d3).default, (d3+d1+d2).default)
-                self.assertAlmostEqual((d3+d1+d2).default, (d1+d2+d3).default)
+            # testing basic functionality for defaults
+            d1 = nd.NumDict(default=i)
+            d2 = nd.NumDict(default=j)
+            self.assertAlmostEqual((d1+d2).default, d1.default+d2.default)
+            d3 = nd.NumDict(default=3.0)
+            self.assertAlmostEqual((d2+d1+d3).default, (d3+d1+d2).default)
+            self.assertAlmostEqual((d3+d1+d2).default, (d1+d2+d3).default)
 
     def test_addition_basic_functionality_keys(self):
         for i, j in linspace(-10, 10):
-            tape = nd.GradientTape()
-            with tape:
-                # testing basic functionality for elements
-                d1 = nd.NumDict({1: i, 2: (i+j)})
-                d2 = nd.NumDict({1: j, 2: (i-j)})
-                d3 = d1+d2
-                self.assertAlmostEqual(d3[1], d1[1]+d2[1])
-                self.assertAlmostEqual(d3[2], d1[2]+d2[2])
+            # testing basic functionality for elements
+            d1 = nd.NumDict({1: i, 2: (i+j)})
+            d2 = nd.NumDict({1: j, 2: (i-j)})
+            d3 = d1+d2
+            self.assertAlmostEqual(d3[1], d1[1]+d2[1])
+            self.assertAlmostEqual(d3[2], d1[2]+d2[2])
 
     def test_addition_basic_functionality_mixed(self):
         for i, j in linspace(-10, 10):
-            tape = nd.GradientTape()
-            with tape:
-                # testing basic functionality with mismatched keys
-                d1 = nd.NumDict({1: i}, default=i)
-                d2 = nd.NumDict({1: j, 2: j}, default=0)
-                d3 = d1+d2
-                self.assertAlmostEqual(d3[1], d1[1]+d2[1])
-                self.assertAlmostEqual(d3[2], d1.default+d2[2])
-                self.assertAlmostEqual(d3.default, d1.default+d2.default)
+            # testing basic functionality with mismatched keys
+            d1 = nd.NumDict({1: i}, default=i)
+            d2 = nd.NumDict({1: j, 2: j}, default=0)
+            d3 = d1+d2
+            self.assertAlmostEqual(d3[1], d1[1]+d2[1])
+            self.assertAlmostEqual(d3[2], d1.default+d2[2])
+            self.assertAlmostEqual(d3.default, d1.default+d2.default)
 
     def test_addition_basic_functionality_nodefault(self):
         for i, j in linspace(-10, 10):
-            tape = nd.GradientTape()
-            with tape:
-                # testing basic functionality with no default and a default
-                d1 = nd.NumDict({1: i, 3: i}, default=i)
-                d2 = nd.NumDict({1: j, 2: j})
-                with self.assertRaises(KeyError):
-                    d1+d2  
+            # testing basic functionality with no default and a default
+            d1 = nd.NumDict({1: i, 3: i}, default=i)
+            d2 = nd.NumDict({1: j, 2: j})
+            with self.assertRaises(KeyError):
+                d1+d2
 
     def test_addition_differentiation(self):
         for i, j in linspace(-10, 10):
@@ -89,46 +81,38 @@ class TestNumdictsAddition(unittest.TestCase):
 class TestNumdictsSubtraction(unittest.TestCase):
     def test_subtraction_basic_functionality_defaults(self):
         for i, j in linspace(-10, 10):
-            tape = nd.GradientTape()
-            with tape:
-                # testing basic functionality for default
-                d1 = nd.NumDict(default=i)
-                d2 = nd.NumDict(default=j)
-                self.assertAlmostEqual((d1-d2).default, d1.default-d2.default)
-                self.assertAlmostEqual((d2-d1).default, d2.default-d1.default)
+            # testing basic functionality for default
+            d1 = nd.NumDict(default=i)
+            d2 = nd.NumDict(default=j)
+            self.assertAlmostEqual((d1-d2).default, d1.default-d2.default)
+            self.assertAlmostEqual((d2-d1).default, d2.default-d1.default)
 
     def test_subtraction_basic_functionality_keys(self):
         for i, j in linspace(-10, 10):
-            tape = nd.GradientTape()
-            with tape:
-                # testing basic functionality for elements
-                d1 = nd.NumDict({1: i, 2: (i+j)})
-                d2 = nd.NumDict({1: j, 2: (i-j)})
-                d3 = d1-d2
-                self.assertAlmostEqual(d3[1], d1[1]-d2[1])
-                self.assertAlmostEqual(d3[2], d1[2]-d2[2])
+            # testing basic functionality for elements
+            d1 = nd.NumDict({1: i, 2: (i+j)})
+            d2 = nd.NumDict({1: j, 2: (i-j)})
+            d3 = d1-d2
+            self.assertAlmostEqual(d3[1], d1[1]-d2[1])
+            self.assertAlmostEqual(d3[2], d1[2]-d2[2])
 
     def test_subtraction_basic_functionality_mixed(self):
         for i, j in linspace(-10, 10):
-            tape = nd.GradientTape()
-            with tape:
-                # testing basic functionality with mismatched keys
-                d1 = nd.NumDict({1: i}, default=i)
-                d2 = nd.NumDict({1: j, 2: j}, default=0)
-                d3 = d1-d2
-                self.assertAlmostEqual(d3[1], d1[1]-d2[1])
-                self.assertAlmostEqual(d3[2], d1.default-d2[2])
-                self.assertAlmostEqual(d3.default, d1.default-d2.default)
+            # testing basic functionality with mismatched keys
+            d1 = nd.NumDict({1: i}, default=i)
+            d2 = nd.NumDict({1: j, 2: j}, default=0)
+            d3 = d1-d2
+            self.assertAlmostEqual(d3[1], d1[1]-d2[1])
+            self.assertAlmostEqual(d3[2], d1.default-d2[2])
+            self.assertAlmostEqual(d3.default, d1.default-d2.default)
 
     def test_subtraction_basic_functionality_nodefault(self):
         for i, j in linspace(-10, 10):
-            tape = nd.GradientTape()
-            with tape:
-                # testing basic functionality with no default and a default
-                d1 = nd.NumDict({1: i, 3: i}, default=i)
-                d2 = nd.NumDict({1: j, 2: j})
-                with self.assertRaises(KeyError):
-                    d1-d2
+            # testing basic functionality with no default and a default
+            d1 = nd.NumDict({1: i, 3: i}, default=i)
+            d2 = nd.NumDict({1: j, 2: j})
+            with self.assertRaises(KeyError):
+                d1-d2
 
     def test_subtraction_differentiation(self):
         for i, j in linspace(-10, 10):
@@ -157,53 +141,45 @@ class TestNumdictsSubtraction(unittest.TestCase):
 class TestNumdictsMultiplication(unittest.TestCase):
     def test_multiplication_basic_functionality_default(self):
         for i, j in linspace(-10, 10):
-            tape = nd.GradientTape()
-            with tape:
-                # testing basic functionality for default
-                d1 = nd.NumDict(default=i)
-                d2 = nd.NumDict(default=j)
-                self.assertAlmostEqual((d1*d2).default, d1.default*d2.default)
-                # testing multiplying by 0
-                d0 = nd.NumDict(default=0.0)
-                self.assertAlmostEqual((d2*d0).default, 0.0)
-                self.assertAlmostEqual((d1*d0).default, 0.0)
-                # testing communative
-                d3 = nd.NumDict(default=3.0)
-                self.assertAlmostEqual((d2*d1*d3).default, (d3*d1*d2).default)
-                self.assertAlmostEqual((d1*d2*d3).default, (d1*d2*d3).default)
+            # testing basic functionality for default
+            d1 = nd.NumDict(default=i)
+            d2 = nd.NumDict(default=j)
+            self.assertAlmostEqual((d1*d2).default, d1.default*d2.default)
+            # testing multiplying by 0
+            d0 = nd.NumDict(default=0.0)
+            self.assertAlmostEqual((d2*d0).default, 0.0)
+            self.assertAlmostEqual((d1*d0).default, 0.0)
+            # testing communative
+            d3 = nd.NumDict(default=3.0)
+            self.assertAlmostEqual((d2*d1*d3).default, (d3*d1*d2).default)
+            self.assertAlmostEqual((d1*d2*d3).default, (d1*d2*d3).default)
 
     def test_multiplication_basic_functionality_keys(self):
         for i, j in linspace(-10, 10):
-            tape = nd.GradientTape()
-            with tape:
-                # testing basic functionality for elements
-                d1 = nd.NumDict({1: i, 2: (i+j)})
-                d2 = nd.NumDict({1: j, 2: (i-j)})
-                d3 = d1*d2
-                self.assertAlmostEqual(d3[1], d1[1]*d2[1])
-                self.assertAlmostEqual(d3[2], d1[2]*d2[2])
+            # testing basic functionality for elements
+            d1 = nd.NumDict({1: i, 2: (i+j)})
+            d2 = nd.NumDict({1: j, 2: (i-j)})
+            d3 = d1*d2
+            self.assertAlmostEqual(d3[1], d1[1]*d2[1])
+            self.assertAlmostEqual(d3[2], d1[2]*d2[2])
 
     def test_multiplication_basic_functionality_mixed(self):
         for i, j in linspace(-10, 10):
-            tape = nd.GradientTape()
-            with tape:
-                # testing basic functionality with mismatched keys
-                d1 = nd.NumDict({1: i}, default=i)
-                d2 = nd.NumDict({1: j, 2: j}, default=0)
-                d3 = d1*d2
-                self.assertAlmostEqual(d3[1], d1[1]*d2[1])
-                self.assertAlmostEqual(d3[2], d1.default*d2[2])
-                self.assertAlmostEqual(d3.default, d1.default*d2.default)
+            # testing basic functionality with mismatched keys
+            d1 = nd.NumDict({1: i}, default=i)
+            d2 = nd.NumDict({1: j, 2: j}, default=0)
+            d3 = d1*d2
+            self.assertAlmostEqual(d3[1], d1[1]*d2[1])
+            self.assertAlmostEqual(d3[2], d1.default*d2[2])
+            self.assertAlmostEqual(d3.default, d1.default*d2.default)
 
     def test_multiplication_basic_functionality_nodefault(self):
         for i, j in linspace(-10, 10):
-            tape = nd.GradientTape()
-            with tape:
-                # testing basic functionality with no default and a default
-                d1 = nd.NumDict({1: i, 3: i}, default=i)
-                d2 = nd.NumDict({1: j, 2: j})
-                with self.assertRaises(KeyError):
-                    d1*d2
+            # testing basic functionality with no default and a default
+            d1 = nd.NumDict({1: i, 3: i}, default=i)
+            d2 = nd.NumDict({1: j, 2: j})
+            with self.assertRaises(KeyError):
+                d1*d2
 
     def test_multiplication_differentiation(self):
         for i, j in linspace(-10, 10):
@@ -349,14 +325,12 @@ class TestNumdictDivision(unittest.TestCase):
 
     def test_truediv_and_rtruediv_basic_functionality_nodefault(self):
         for i, j in linspace(-10, 10):
-            tape = nd.GradientTape()
-            with tape:
-                # testing basic functionality with mismatched keys
-                d1 = nd.NumDict({1: i, 3: i}, default=i)
-                d2 = nd.NumDict({1: j, 2: j})
-                if(d1[1]*d1.default*d2[1]*d2[2] != 0):
-                    with self.assertRaises(KeyError):
-                        d1/d2
+            # testing basic functionality with mismatched keys
+            d1 = nd.NumDict({1: i, 3: i}, default=i)
+            d2 = nd.NumDict({1: j, 2: j})
+            if(d1[1]*d1.default*d2[1]*d2[2] != 0):
+                with self.assertRaises(KeyError):
+                    d1/d2
 
     def test_truediv_and_rtruediv_differentiation(self):
         for i, j in linspace(-10, 10):
@@ -414,77 +388,69 @@ class TestNumdictDivision(unittest.TestCase):
 class TestNumdictsPower(unittest.TestCase):
     def test_pow_and_rpow_basic_functionality_defaults(self):
         for i, j in linspace(-10, 10):
-            tape = nd.GradientTape()
-            with tape:
-                # testing basic functionality for default
-                d1 = nd.NumDict(default=i)
-                d2 = nd.NumDict(default=j)
-                if(d1.default >= 0) or (d1.default < 0 and d2.default.is_integer()):
-                    if (d1.default != 0 or d2.default >= 0):
+            # testing basic functionality for default
+            d1 = nd.NumDict(default=i)
+            d2 = nd.NumDict(default=j)
+            if(d1.default >= 0) or (d1.default < 0 and d2.default.is_integer()):
+                if (d1.default != 0 or d2.default >= 0):
+                    self.assertAlmostEqual(
+                        (d1 ** d2).default, d1.default ** d2.default)
+                    self.assertAlmostEqual(NumDict.__pow__(
+                        d1, d2).default, (d1**d2).default)
+                    self.assertAlmostEqual(NumDict.__pow__(
+                        d1, d2).default, NumDict.__rpow__(d2, d1).default)
+                else:
+                    with self.assertRaises(ZeroDivisionError):
                         self.assertAlmostEqual(
                             (d1 ** d2).default, d1.default ** d2.default)
                         self.assertAlmostEqual(NumDict.__pow__(
                             d1, d2).default, (d1**d2).default)
                         self.assertAlmostEqual(NumDict.__pow__(
                             d1, d2).default, NumDict.__rpow__(d2, d1).default)
-                    else:
-                        with self.assertRaises(ZeroDivisionError):
-                            self.assertAlmostEqual(
-                                (d1 ** d2).default, d1.default ** d2.default)
-                            self.assertAlmostEqual(NumDict.__pow__(
-                                d1, d2).default, (d1**d2).default)
-                            self.assertAlmostEqual(NumDict.__pow__(
-                                d1, d2).default, NumDict.__rpow__(d2, d1).default)
 
     def test_pow_and_rpow_basic_functionality_keys(self):
         for i, j in linspace(-10, 10):
-            tape = nd.GradientTape()
-            with tape:
-                # testing basic functionality for elements
-                d1 = nd.NumDict({1: i, 2: (i+j)})
-                d2 = nd.NumDict({1: j, 2: (i-j)})
-                if (d1[1] >= 0 and d1[2] >= 0) or ((d1[1] < 0 and d2[1].is_integer()) and (d1[2] < 0 and d2[2].is_integer())):
-                    d3 = d1 ** d2
-                    self.assertAlmostEqual(d3[1], d1[1] ** d2[1])
-                    self.assertAlmostEqual(d3[2], d1[2] ** d2[2])
-                    self.assertAlmostEqual(NumDict.__pow__(d1, d2)[1], d3[1])
-                    self.assertAlmostEqual(NumDict.__pow__(d1, d2)[2], d3[2])
-                    self.assertAlmostEqual(NumDict.__pow__(d1, d2)[1],
-                                           NumDict.__rpow__(d2, d1)[1])
-                    self.assertAlmostEqual(NumDict.__pow__(d1, d2)[2],
-                                           NumDict.__rpow__(d2, d1)[2])
+            # testing basic functionality for elements
+            d1 = nd.NumDict({1: i, 2: (i+j)})
+            d2 = nd.NumDict({1: j, 2: (i-j)})
+            if (d1[1] >= 0 and d1[2] >= 0) or ((d1[1] < 0 and d2[1].is_integer()) and (d1[2] < 0 and d2[2].is_integer())):
+                d3 = d1 ** d2
+                self.assertAlmostEqual(d3[1], d1[1] ** d2[1])
+                self.assertAlmostEqual(d3[2], d1[2] ** d2[2])
+                self.assertAlmostEqual(NumDict.__pow__(d1, d2)[1], d3[1])
+                self.assertAlmostEqual(NumDict.__pow__(d1, d2)[2], d3[2])
+                self.assertAlmostEqual(NumDict.__pow__(d1, d2)[1],
+                                       NumDict.__rpow__(d2, d1)[1])
+                self.assertAlmostEqual(NumDict.__pow__(d1, d2)[2],
+                                       NumDict.__rpow__(d2, d1)[2])
 
     def test_pow_and_rpow_basic_functionality_mixed(self):
         for i, j in linspace(-10, 10):
-            tape = nd.GradientTape()
-            with tape:
-                # testing basic functionality for elements
-                d1 = nd.NumDict({1: i}, default=i)
-                d2 = nd.NumDict({1: j, 2: j}, default=1)
-                if (d1[1] >= 0 and d1.default > 0) or ((d1[1] < 0 and d2[1].is_integer()) and (d1.default < 0 and d2[2].is_integer())):
-                    d3 = d1 ** d2
-                    self.assertAlmostEqual(d3[1], d1[1] ** d2[1])
-                    self.assertAlmostEqual(d3[2], d1[2] ** d2[2])
-                    self.assertAlmostEqual(d3.default, d1.default**d2.default)
-                    self.assertAlmostEqual(
-                        NumDict.__pow__(d1, d2)[1], d3[1])
-                    self.assertAlmostEqual(
-                        NumDict.__pow__(d1, d2)[2], d3[2])
-                    self.assertAlmostEqual(NumDict.__pow__(d1, d2)[1],
-                                           NumDict.__rpow__(d2, d1)[1])
-                    self.assertAlmostEqual(NumDict.__pow__(d1, d2)[2],
-                                           NumDict.__rpow__(d2, d1)[2])
+            # testing basic functionality for elements
+            d1 = nd.NumDict({1: i}, default=i)
+            d2 = nd.NumDict({1: j, 2: j}, default=1)
+            if (d1[1] >= 0 and d1.default > 0) or ((d1[1] < 0 and d2[1].is_integer()) and (d1.default < 0 and d2[2].is_integer())):
+                d3 = d1 ** d2
+                self.assertAlmostEqual(d3[1], d1[1] ** d2[1])
+                self.assertAlmostEqual(d3[2], d1[2] ** d2[2])
+                self.assertAlmostEqual(d3.default, d1.default**d2.default)
+                self.assertAlmostEqual(
+                    NumDict.__pow__(d1, d2)[1], d3[1])
+                self.assertAlmostEqual(
+                    NumDict.__pow__(d1, d2)[2], d3[2])
+                self.assertAlmostEqual(NumDict.__pow__(d1, d2)[1],
+                                       NumDict.__rpow__(d2, d1)[1])
+                self.assertAlmostEqual(NumDict.__pow__(d1, d2)[2],
+                                       NumDict.__rpow__(d2, d1)[2])
 
     def test_pow_and_rpow_basic_functionality_nodefault(self):
         for i, j in linspace(-10, 10):
-            tape = nd.GradientTape()
-            with tape:
-                # testing basic functionality with mismatched keys
-                d1 = nd.NumDict({1: i, 3: i}, default=i)
-                d2 = nd.NumDict({1: j, 2: j})
-                if (d1[1] >= 0 and d1.default > 0) or ((d1[1] < 0 and d2[1].is_integer()) and (d1.default < 0 and d2[2].is_integer())):
-                    with self.assertRaises(KeyError):
-                        d1**d2  
+            # testing basic functionality with mismatched keys
+            d1 = nd.NumDict({1: i, 3: i}, default=i)
+            d2 = nd.NumDict({1: j, 2: j})
+            if (d1[1] >= 0 and d1.default > 0) or ((d1[1] < 0 and d2[1].is_integer()) and (d1.default < 0 and d2[2].is_integer())):
+                with self.assertRaises(KeyError):
+                    d1**d2
 
     def test_pow_and_rpow_differentiation(self):
         for i, j in linspace(-1, 10):
