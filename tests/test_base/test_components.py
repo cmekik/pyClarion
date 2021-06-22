53a05ecf9b9c96e342126624fa15cfc99867da56
Zeng <78892812+HZeng3@users.noreply.github.com>
2021-06-21 22:57:42 -0400

    domain test 1

--- tests/test_base/test_components.py
+++ tests/test_base/test_components.py
@@ -1,5 +1,6 @@
 import pyClarion.base as clb
 import pyClarion.numdicts as nd
+import pyClarion.domain as Domain
 
 import unittest
 import unittest.mock as mock
@@ -39,3 +40,67 @@ class TestProcess(unittest.TestCase):
 
 class TestWrappedProcess(unittest.TestCase):
     pass
+
+class TestDomainMethods(unittest.TestCase):
+
+    def test_lock_disallows_mutation_of_domain(self):
+
+        dom = Domain(
+        features=(
+            feature("x", "a"),
+            feature("y", "b"),
+            feature("z", "c"),
+            feature("d"),
+            feature("e")
+        )
+
+        dom.lock()
+
+    )
+
+    def test_disjoint_recognize_overlaps(self):
+
+        dom1 = Domain(
+        features=(
+            feature("x", "a"),
+            feature("y", "b"),
+            feature("z", "c"),
+            feature("d"),
+            feature("e")
+        )
+
+        dom2 = Domain(
+        features=(
+            feature("z", "a"),
+            feature("x", "b"),
+            feature("y", "c"),
+            feature("f"),
+            feature("g")
+        )
+
+        dom3 = Domain(
+        features=(
+            feature("x", "d"),
+            feature("b", "y"),
+            feature("z", "c"),
+            feature("f")
+        )
+
+        dom0 = Domain(
+        features=(
+        )
+
+        dom4 = Domain(
+        features=(
+            feature("1", "2"),
+            feature("3", "4"),
+            feature("5"),
+            feature("6")
+        )
+
+        self.assertEqual(Domain.disjoint(dom1, dom2), True)
+        self.assertEqual(Domain.disjoint(dom1, dom3), False)
+        self.assertEqual(Domain.disjoint(dom1, dom2, dom4), True)
+        self.assertEqual(Domain.disjoint(dom1, dom0), True)
+        self.assertEqual(Domain.disjoint(dom1), True)
+        self.assertEqual(Domain.disjoint(), True)
