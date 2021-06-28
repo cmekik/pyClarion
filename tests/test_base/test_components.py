0c27fbe36f6cd2be112ee8cfe5d249ea1fb95a85
Zeng <78892812+HZeng3@users.noreply.github.com>
2021-06-28 04:06:46 -0400

    Update test_components.py

--- tests/test_base/test_components.py
+++ tests/test_base/test_components.py
@@ -1,6 +1,6 @@
 import pyClarion.base as clb
 import pyClarion.numdicts as nd
-import pyClarion.domain as Domain
+from pyClarion import feature
 
 import unittest
 import unittest.mock as mock
@@ -43,64 +43,97 @@ class TestWrappedProcess(unittest.TestCase):
 
 class TestDomainMethods(unittest.TestCase):
 
+    def test_config(self):
+
+        dom = clb.Domain(
+            features=(
+                feature("x", "a"),
+                feature("y", "b"),
+                feature("z", "c"),
+                feature("d"),
+                feature("e")
+            )
+        )
+
+        with dom.config():
+            dom._features[0] = feature("")
+
     def test_lock_disallows_mutation_of_domain(self):
 
-        dom = Domain(
-        features=(
-            feature("x", "a"),
-            feature("y", "b"),
-            feature("z", "c"),
-            feature("d"),
-            feature("e")
+        dom = clb.Domain(
+            features=(
+                feature("x", "a"),
+                feature("y", "b"),
+                feature("z", "c"),
+                feature("d"),
+                feature("e")
+            )
         )
 
         dom.lock()
+        # how to modify dom?
+        
+        #dom = clb.Domain(
+        #    features=(
+        #        feature("d")
+        #    )
+        #)
+        # domain __init__ doesn't accept single argument (single feature)
 
-    )
+        #fs = dom.features() # tuple not callable?
+
 
     def test_disjoint_recognize_overlaps(self):
 
-        dom1 = Domain(
-        features=(
-            feature("x", "a"),
-            feature("y", "b"),
-            feature("z", "c"),
-            feature("d"),
-            feature("e")
+        dom1 = clb.Domain(
+            features=(
+                feature("x", "a"),
+                feature("y", "b"),
+                feature("z", "c"),
+                feature("d"),
+                feature("e")
+            )
         )
 
-        dom2 = Domain(
-        features=(
-            feature("z", "a"),
-            feature("x", "b"),
-            feature("y", "c"),
-            feature("f"),
-            feature("g")
+        dom2 = clb.Domain(
+            features=(
+                feature("z", "a"),
+                feature("x", "b"),
+                feature("y", "c"),
+                feature("f"),
+                feature("g")
+            )
         )
 
-        dom3 = Domain(
-        features=(
-            feature("x", "d"),
-            feature("b", "y"),
-            feature("z", "c"),
-            feature("f")
+        dom3 = clb.Domain(
+            features=(
+                feature("x", "d"),
+                feature("b", "y"),
+                feature("z", "c"),
+                feature("f")
+            )
         )
 
-        dom0 = Domain(
-        features=(
+        dom0 = clb.Domain(
+            features=()
         )
 
-        dom4 = Domain(
-        features=(
-            feature("1", "2"),
-            feature("3", "4"),
-            feature("5"),
-            feature("6")
+        dom4 = clb.Domain(
+            features=(
+                feature("1", "2"),
+                feature("3", "4"),
+                feature("5"),
+                feature("6")
+            )
         )
 
-        self.assertEqual(Domain.disjoint(dom1, dom2), True)
-        self.assertEqual(Domain.disjoint(dom1, dom3), False)
-        self.assertEqual(Domain.disjoint(dom1, dom2, dom4), True)
-        self.assertEqual(Domain.disjoint(dom1, dom0), True)
-        self.assertEqual(Domain.disjoint(dom1), True)
-        self.assertEqual(Domain.disjoint(), True)
+        self.assertEqual(clb.Domain.disjoint(dom1, dom2), True)
+        self.assertEqual(clb.Domain.disjoint(dom1, dom3), False)
+        self.assertEqual(clb.Domain.disjoint(dom1, dom2, dom4), True)
+        self.assertEqual(clb.Domain.disjoint(dom1, dom0), True)
+        #self.assertEqual(clb.Domain.disjoint(dom1), True) # ???
+        #self.assertEqual(clb.Domain.disjoint(), True) # argument cannot be empty
+
+
+if __name__ == "__main__":
+    unittest.main()
