6467bea21a6229a5718fec5df71b2351e268279b
Zeng <78892812+HZeng3@users.noreply.github.com>
2021-07-21 17:22:10 -0400

    Finalized testing domain
    Ready for pull request

--- tests/test_base/test_components.py
+++ tests/test_base/test_components.py
@@ -44,45 +44,33 @@ class TestWrappedProcess(unittest.TestCase):
 
 class TestDomainMethods(unittest.TestCase):
 
-    #@mock.patch.object(clb.Domain, "config")
-    def test_config(self):
+    def test_config_calls_update_only_at_end_of_with_block(self):
 
-        with mock.patch('pyClarion.base.Domain', new_callable=PropertyMock) as mockDom:
-            mockDom._config = ("A", "B", "C")
-            mockDom = clb.Domain(
-                features=(
-                    feature("x", "a"),
-                    feature("y", "b"),
-                    feature("z", "c"),
-                    feature("d"),
-                    feature("e")
+        with mock.patch('pyClarion.base.Domain.update'):
+            with mock.patch('pyClarion.base.Domain._config', ("A", "B", "C")):
+                mockDom = clb.Domain(
+                    features=(
+                        feature("x", "a"),
+                        feature("y", "b"),
+                        feature("z", "c"),
+                        feature("d"),
+                        feature("e")
+                    )
                 )
-            )
 
-            with mockDom.config():
-                mockDom.update.assert_not_called()
-                mockDom.A = "a"
-                mockDom.update.assert_not_called()
-                mockDom.B = "b"
-                mockDom.update.assert_not_called()
-                mockDom.C = "c"
-            #mockDom.update.assert_not_called()???
-
-
-        #dom = mock()
-
-        #with mock.patch.object(dom, "_config", "a"):
-            #with dom.config():
-
-                #dom.update.assert_not_called()
-
-        #with dom.config():
-            #update not called
-        #update called with end of "with block"
+                with mockDom.config():
+                    mockDom.update.assert_not_called()
+                    mockDom.A = "a"
+                    mockDom.update.assert_not_called()
+                    mockDom.B = "b"
+                    mockDom.update.assert_not_called()
+                    mockDom.C = "c"
+                    mockDom.update.assert_not_called()
+                mockDom.update.assert_called()
 
     def test_lock_disallows_mutation_of_domain(self):
 
-        with mock.patch('pyClarion.base.Domain', new_callable=PropertyMock) as mockDom:
+        with mock.patch('pyClarion.base.Domain._config', ("A", "B", "C")):
             mockDom = clb.Domain(
                 features=(
                     feature("x", "a"),
@@ -92,13 +80,17 @@ class TestDomainMethods(unittest.TestCase):
                     feature("e")
                 )
             )
-            mockDom._config = ("A", "B", "C")
 
             mockDom.lock()
-            # how to modify dom?
-            with mockDom.config():
-                mockDom.A = "a"
+            with self.subTest(msg="run_with_config()"):
+                with mockDom.config():
 
+                    with self.assertRaisesRegex(RuntimeError, "Cannot mutate locked domain."):
+                        mockDom.A = "a"
+
+            with self.subTest(msg="run_without_config()"):
+                with self.assertRaisesRegex(RuntimeError, "Cannot mutate locked domain."):
+                        mockDom.B = "b"
 
     def test_disjoint_recognize_overlaps(self):
 
@@ -144,13 +136,24 @@ class TestDomainMethods(unittest.TestCase):
             )
         )
 
-        self.assertEqual(clb.Domain.disjoint(dom1, dom2), True)
-        self.assertEqual(clb.Domain.disjoint(dom1, dom3), False)
-        self.assertEqual(clb.Domain.disjoint(dom1, dom2, dom4), True)
-        self.assertEqual(clb.Domain.disjoint(dom1, dom0), True)
-        self.assertEqual(clb.Domain.disjoint(dom1), True)
-        with self.assertRaises(ValueError):
-            clb.Domain.disjoint()
+        with self.subTest(msg="different domains with same size"):
+            self.assertEqual(clb.Domain.disjoint(dom1, dom2), True)
+
+        with self.subTest(msg="different domains with different size"):
+            self.assertEqual(clb.Domain.disjoint(dom1, dom3), False)
+
+        with self.subTest(msg="more than 2 domains as argument"):
+            self.assertEqual(clb.Domain.disjoint(dom1, dom2, dom4), True)
+
+        with self.subTest(msg="one of the domains being empty"):
+            self.assertEqual(clb.Domain.disjoint(dom1, dom0), True)
+
+        with self.subTest(msg="only 1 domain as argument"):
+            self.assertEqual(clb.Domain.disjoint(dom1), True)
+            
+        with self.subTest(msg="without argument"):
+            with self.assertRaisesRegex(ValueError, "disjoint\(\) doesn't accept 0 argument"):
+                clb.Domain.disjoint()
 
 
 if __name__ == "__main__":
