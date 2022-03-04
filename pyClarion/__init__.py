074e241741795a0cae74f08ae07c126b06d0f331
Can Serif Mekik <can.mekik@gmail.com>
2022-03-04 17:29:35 -0500

    reexposed gradient tape

--- pyClarion/__init__.py
+++ pyClarion/__init__.py
@@ -2,7 +2,7 @@ from .base import dimension, feature, chunk, rule, Module, Structure
 from .components import (Repeat, Receptors, Actions, CAM, Shift, 
     BoltzmannSampler, ActionSampler, BottomUp, TopDown, AssociativeRules, 
     ActionRules, BLATracker, Store, GoalStore, Flags, Slots, Gates, DimFilter)
-from .numdicts import NumDict
+from .numdicts import NumDict, GradientTape
 from .utils import pprint, pformat, load, inspect
 
 __all__ = [
@@ -10,5 +10,5 @@ __all__ = [
     "Repeat", "Receptors", "Actions", "CAM", "Shift", "BoltzmannSampler", 
     "ActionSampler", "BottomUp", "TopDown", "AssociativeRules", "ActionRules", 
     "BLATracker", "Store", "GoalStore", "Flags", "Slots", "Gates", "DimFilter", 
-    "NumDict", "pprint", "pformat", "load", "inspect"
+    "NumDict", "GradientTape", "pprint", "pformat", "load", "inspect"
 ]
