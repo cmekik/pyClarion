8cf3428821bff71562a2cb804c5b7f782750c446
Can Serif Mekik <can.mekik@gmail.com>
2024-10-19 18:37:47 -0400

    reworked knowledge grammar

--- pyClarion/components/knowledge.py
+++ pyClarion/components/knowledge.py
@@ -50,17 +50,22 @@ class Atom(KeySpace):
     def __pos__(self: Self) -> Self:
         return self
 
-    def __matmul__(self, other: "Atom | Var | Iterable[Atom]") -> "Chunk":
+    def __pow__(self, other: "Atom | Var | Iterable[Atom]") -> "Chunk":
         if isinstance(other, (Atom, Var)):
             return Chunk({(self, other): 1.0})
         else:
             return Chunk({(self, atom): 1.0 for atom in other})
 
+    def __rpow__(self, other: "Atom | Var") -> "Chunk":
+        if isinstance(other, (Atom, Var)):
+            return Chunk({(other, self): 1.0})
+        return NotImplemented
+
 
 class Chunk(Atom):
-    _dyads_: dict[tuple[Atom, "Atom | Var"], float]
+    _dyads_: "dict[tuple[Atom | Var, Atom | Var], float]"
     
-    def __init__(self, dyads: dict[tuple[Atom, "Atom | Var"], float]) -> None:
+    def __init__(self, dyads: "dict[tuple[Atom | Var, Atom | Var], float]") -> None:
         super().__init__()
         self._dyads_ = dyads
 
@@ -79,6 +84,13 @@ class Chunk(Atom):
         if isinstance(other, Chunk):
             dyads = self._dyads_.copy()
             dyads.update(other._dyads_)
+            return Chunk(dyads)
+        return NotImplemented
+
+    def __sub__(self, other: "Chunk") -> "Chunk":
+        if isinstance(other, Chunk):
+            dyads = self._dyads_.copy()
+            dyads.update({d: -w for d, w in other._dyads_.items()})
             return Chunk(dyads)
         return NotImplemented
 
@@ -132,3 +144,12 @@ class Triads(Index):
             raise ValueError("Mismatched keyspaces")
         ref = path(sort1).link(path(sort2), 0).link(path(sort3), 0)
         return cls(ksp, ref, (1, 1, 1))        
+
+
+magoo = Sort()
+magoo_var = Var(magoo)
+ch  = (
+    - magoo.a ** magoo.b 
+    + (magoo_var := Var(magoo)) ** magoo.d 
+    >>
+    + magoo.e ** magoo_var)
