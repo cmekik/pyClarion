a1cb5e864a86a1923b512c2496e93da6e231ea91
Can Serif Mekik <can.mekik@gmail.com>
2025-02-03 00:53:41 -0500

    fix to numdict __init__

--- pyClarion/numdicts/__init__.py
+++ pyClarion/numdicts/__init__.py
@@ -1,6 +1,7 @@
 from .exc import ValidationError
 from .keys import Key, KeyForm
-from .keyspaces import (KeySpaceBase, KeySpace, Index, root, path, parent, bind, crawl)
+from .keyspaces import (KeySpaceBase, KeySpace, Index, root, path, parent, 
+    bind, crawl)
 from .numdicts import NumDict, numdict
 
 __all__ = ["ValidationError", "Key", "KeyForm", "KeySpaceBase", "KeySpace", 
