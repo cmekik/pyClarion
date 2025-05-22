1e072c034dcd5e55363479bfcab3a09be96cf1ec
Can Serif Mekik <can.mekik@gmail.com>
2025-05-22 15:00:29 -0400

    forgot rules.py

---
+++ pyClarion/components/rules.py
@@ -0,0 +1,131 @@
+from typing import Sequence
+from datetime import timedelta
+import logging
+
+from .layers import Layer
+from ..system import Process, Site, UpdateSort, Event, Priority
+from ..knowledge import Rules, Rule, Family, Chunks
+from ..numdicts import keyform, NumDict
+from ..numdicts.ops.base import Unary
+
+
+class RuleStore(Process):
+    """
+    A rule store. 
+
+    Maintains a collection of rules.
+    """
+
+    rules: Rules
+    lhs: Chunks
+    rhs: Chunks
+    riw: Site
+    lhw: Site
+    rhw: Site
+
+    def __init__(self, 
+        name: str, 
+        r: Family,
+        lhs: Chunks,
+        rhs: Chunks | None = None
+    ) -> None:
+        rhs = rhs if rhs is not None else lhs
+        super().__init__(name)
+        self.system.check_root(r, lhs, rhs)
+        self.rules = Rules(); r[name] = self.rules
+        self.lhs = lhs
+        self.rhs = rhs
+        idx_r = self.system.get_index(keyform(self.rules))
+        idx_lhs = self.system.get_index(keyform(lhs))
+        idx_rhs = self.system.get_index(keyform(rhs))
+        self.main = Site(idx_r, {}, c=0.0)
+        self.riw = Site(idx_r * idx_r, {}, c=0.0)
+        self.lhw = Site(idx_lhs * idx_r, {}, c=0.0)
+        self.rhw = Site(idx_r * idx_rhs, {}, c=0.0)
+
+    def resolve(self, event: Event) -> None:
+        new_rules = [rule 
+            for ud in event.updates 
+                if isinstance(ud, UpdateSort) and ud.sort is self.rules 
+                    for rule in ud.add]
+        if new_rules:
+            if event.source == self.encode \
+                and self.system.logger.isEnabledFor(logging.DEBUG):
+                self.log_encoding(new_rules)
+            self.system.schedule(self.encode_weights(*new_rules))
+
+    def log_encoding(self, rules: Sequence[Rule]) -> None:
+        data = [f"    Added the following new rule(s)"]
+        for r in rules:
+            data.append(str(r).replace("\n", "\n    "))
+        self.system.logger.debug("\n    ".join(data))
+
+    def encode(self, *rules: Rule, 
+        dt: timedelta = timedelta(),
+        priority: int = Priority.LEARNING
+    ) -> Event:
+        """Encode a collection of new rules."""
+        new_rules = []
+        new_lhs_chunks = []
+        new_rhs_chunks = []
+        for rule in rules:
+            for i, chunk in enumerate(rule._chunks_):
+                chunk_instances = list(chunk._instantiations_())
+                chunk._instances_.update(chunk_instances)
+                if self.lhs is self.rhs:
+                    new_lhs_chunks.append(chunk)
+                    new_lhs_chunks.extend(chunk_instances)
+                elif i < len(rule._chunks_) - 1:
+                    new_lhs_chunks.append(chunk)
+                    new_lhs_chunks.extend(chunk_instances)
+                else:
+                    new_rhs_chunks.append(chunk)
+                    new_rhs_chunks.extend(chunk_instances)
+            rule_instances = list(rule._instantiations_())
+            rule._instances_.update(rule_instances)
+            new_rules.append(rule)
+            new_rules.extend(rule_instances)
+        updates = [UpdateSort(self.rules, add=tuple(new_rules)),
+            UpdateSort(self.lhs, add=tuple(new_lhs_chunks))]
+        if new_rhs_chunks:
+            updates.append(UpdateSort(self.rhs, add=tuple(new_rhs_chunks)))
+        return Event(self.encode, updates, dt, priority)
+
+    def encode_weights(self, 
+        *rules: Rule, 
+        dt: timedelta = timedelta(), 
+        priority=Priority.LEARNING
+    ) -> Event:
+        riw, lhw, rhw = {}, {}, {}
+        for rule in rules:
+            data = rule._compile_()
+            riw.update(data["riw"])
+            lhw.update(data["lhw"])
+            rhw.update(data["rhw"])
+        return Event(self.encode_weights,
+            [self.riw.update(riw, Site.write_inplace),
+             self.lhw.update(lhw, Site.write_inplace),
+             self.rhw.update(rhw, Site.write_inplace)],
+            dt, priority)
+    
+    def lhs_layer(self, 
+        name: str, 
+        *, 
+        func: Unary[NumDict] | None = None, 
+        l: int = 1
+    ) -> Layer:
+        layer = Layer(name, self.lhs, self.rules)
+        layer.weights = self.lhw
+        #layer.bias = self.bias
+        return layer
+
+    def rhs_layer(self, 
+        name: str, 
+        *, 
+        func: Unary[NumDict] | None = None, 
+        l: int = 1
+    ) -> Layer:
+        layer = Layer(name, self.rules, self.rhs)
+        layer.weights = self.rhw
+        #layer.bias = self.bias
+        return layer
