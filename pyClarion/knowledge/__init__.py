5235fa3f3f9d333f576f291341b722a8b9163559
Can Serif Mekik <can.mekik@gmail.com>
2025-08-26 10:33:52 -0400

    added Semantic + Structural type aliases

--- pyClarion/knowledge/__init__.py
+++ pyClarion/knowledge/__init__.py
@@ -4,9 +4,15 @@ from .sorts import Buses, Atoms, Compounds, Chunks, Rules
 from .families import DataFamily, AtomFamily, ChunkFamily, RuleFamily, BusFamily
 
 
+Semantic = (DataFamily | AtomFamily | ChunkFamily | RuleFamily | Atoms 
+    | Chunks | Rules | Atom | Chunk | Rule)
+Structural = BusFamily | Buses | Bus
+
+
 __all__ = [
     "Symbol", "Term", "Sort", "Var", "Family", "Root", 
     "Bus", "Atom", "Compound", "Chunk", "Rule",
     "Buses", "Atoms", "Compounds", "Chunks", "Rules",
-    "DataFamily", "AtomFamily", "ChunkFamily", "RuleFamily", "BusFamily"
+    "DataFamily", "AtomFamily", "ChunkFamily", "RuleFamily", "BusFamily",
+    "Semantic", "Structural"
 ]
